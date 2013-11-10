a2f37cc68b4195b6b9824dbbe1c01ff06ca097f8
tinker10 <ewang7100@gmail.com>
2013-11-10 11:02:17 -0800

    Implemented hard wall boundary conditions.

--- bullet.js
+++ bullet.js
@@ -4,28 +4,53 @@ d3.anneal = function() {
   var a = 380,
       b = 30,
       label_array = [],
+      w = 1, // box width
+      h = 1, // box width
       anneal = {};
+
+  energy = function(index) {
+      var m = label_array.length, 
+          ener = 0;
+
+      for (var i = 0; i < m; i++) {
+        if (i != index) {
+          var dist = Math.sqrt();
+        }
+      }
+  };
 
   anneal.sweep = function() {
       var m = label_array.length;
       for (var i = 0; i < m; i++) {
           // console.log(label_array[i].name)
-          label_array[i].x += (Math.random() - 0.5) * 5;
-          label_array[i].y += (Math.random() - 0.5) * 5;
+
+          var x_old = label_array[i].x;
+          var y_old = label_array[i].y;
+
+          var old_energy, new_energy;
+
+          label_array[i].x += (Math.random() - 0.5) * 500;
+          label_array[i].y += (Math.random() - 0.5) * 500;
+
+          // hard wall boundaries
+          if (label_array[i].x > w) label_array[i].x = x_old;
+          if (label_array[i].x < 0) label_array[i].x = x_old;
+          if (label_array[i].y > h) label_array[i].y = y_old;
+          if (label_array[i].y < 0) label_array[i].y = y_old;
       }
   };
 
   anneal.width = function(x) {
-    if (!arguments.length) return a;
-    a = x;
-    // console.log(a)
+    if (!arguments.length) return w;
+    w = x;
+    // console.log("width" + w);
     return anneal;
   };
 
   anneal.height = function(x) {
-    if (!arguments.length) return b;
-    b = x;    
-    // console.log(b)
+    if (!arguments.length) return h;
+    h = x;    
+    // console.log("width" + h);
     return anneal;
   };
 
