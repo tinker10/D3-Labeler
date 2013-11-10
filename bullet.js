c43a7f74e757bf11a9c671572cf4b1198a0bfa93
tinker10 <ewang7100@gmail.com>
2013-11-10 13:35:26 -0800

    Added framework to energy penalties.

--- bullet.js
+++ bullet.js
@@ -3,50 +3,105 @@
 d3.anneal = function() {
   var a = 380,
       b = 30,
-      label_array = [],
+      lab = [],
+      anc = [],
       w = 1, // box width
       h = 1, // box width
       anneal = {};
 
   energy = function(index) {
-      var m = label_array.length, 
-          ener = 0;
+      var m = lab.length, 
+          ener = 0,
+          dx = 0,
+          dy = 0,
+          dist = 0
+          overlap = true;
+
+      // penalty for length of leader line
+      dx = anc[index].x - lab[index].x;
+      dy = anc[index].y - lab[index].y;
+      dist = Math.sqrt(dx * dx + dy * dy);
+      // more stuff here
 
       for (var i = 0; i < m; i++) {
         if (i != index) {
-          var dx = label_array[i].x - label_array[index].x;
-          var dy = label_array[i].y - label_array[index].y;
-          var dist = Math.sqrt(dz * dx + dy * dy);
-          ener += 5;
+
+          // penalty for overlap of leader lines
+          overlap = intersect(anc[index].x, lab[index].x, anc[i].x, lab[i].x,
+                          anc[index].y, lab[index].y, anc[i].y, lab[i].y);
+          // if (overlap) continue
+
+          // penalty for label-label overlap
+          dx = lab[i].x - lab[index].x;
+          dy = lab[i].y - lab[index].y;
+          dist = Math.sqrt(dx * dx + dy * dy);
+          // if (dist < 50) ener += 500;
         }
+        
+        // penalty for label-anchor overlap
+        dx = anc[i].x - lab[index].x;
+        dy = anc[i].y - lab[index].y;
+        dist = Math.sqrt(dx * dx + dy * dy);
+        // if (dist < 50) ener += 500;
       }
+      return ener;
   };
 
-  anneal.mcmove = function() {
+  mcmove = function() {
 
       // random number between 0 and 
-      var i = Math.floor((Math.random()*label_array.length)); 
+      var i = Math.floor((Math.random()*lab.length)); 
 
-      // console.log(label_array[i].name)
+      // console.log(lab[i].name)
 
-      var x_old = label_array[i].x;
-      var y_old = label_array[i].y;
+      var x_old = lab[i].x;
+      var y_old = lab[i].y;
 
-      var old_energy, new_energy;
+      var old_energy = energy(i);
 
-      label_array[i].x += (Math.random() - 0.5) * 50;
-      label_array[i].y += (Math.random() - 0.5) * 50;
+      lab[i].x += (Math.random() - 0.5) * 50;
+      lab[i].y += (Math.random() - 0.5) * 50;
 
       // hard wall boundaries
-      if (label_array[i].x > w) label_array[i].x = x_old;
-      if (label_array[i].x < 0) label_array[i].x = x_old;
-      if (label_array[i].y > h) label_array[i].y = y_old;
-      if (label_array[i].y < 0) label_array[i].y = y_old;
+      if (lab[i].x > w) lab[i].x = x_old;
+      if (lab[i].x < 0) lab[i].x = x_old;
+      if (lab[i].y > h) lab[i].y = y_old;
+      if (lab[i].y < 0) lab[i].y = y_old;
+
+      var new_energy = energy(i);
+      var delta_energy = new_energy - old_energy;
+
+      if (Math.random() < Math.exp(-delta_energy / 1.0)) {
+        // acceptance stuff here
+      } else {
+        lab[i].x = x_old;
+        lab[i].y = y_old;
+      }
+
   };
 
+  intersect = function(x1, x2, x3, x4, y1, y2, y3, y4) {
+    // from http://paulbourke.net/geometry/lineline2d/
+
+    var mua, mub;
+    var denom, numera, numerb;
+
+    denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+    numera = (x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3);
+    numerb = (x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3);
+
+    /* Is the intersection along the the segments */
+    mua = numera / denom;
+    mub = numerb / denom;
+    if (!(mua < 0 || mua > 1 || mub < 0 || mub > 1)) {
+        return true;
+    }
+    return false;
+}
+
   anneal.sweep = function() {
-      var m = label_array.length;
-      for (var i = 0; i < m; i++) { anneal.mcmove(); }
+      var m = lab.length;
+      for (var i = 0; i < m; i++) { mcmove(); }
   };
 
   anneal.width = function(x) {
@@ -64,8 +119,14 @@ d3.anneal = function() {
   };
 
   anneal.nodes = function(x) {
-    if (!arguments.length) return label_array;
-    label_array = x;
+    if (!arguments.length) return lab;
+    lab = x;
+    return anneal;
+  };
+
+  anneal.anchor = function(x) {
+    if (!arguments.length) return anc;
+    anc = x;
     return anneal;
   };
 
