7035d3bb15d280cba710bf8914ccfeb762fd0c9d
tinker10 <ewang7100@gmail.com>
2013-11-10 11:21:38 -0800

    Separated sweep and mcmove.

--- bullet.js
+++ bullet.js
@@ -14,30 +14,39 @@ d3.anneal = function() {
 
       for (var i = 0; i < m; i++) {
         if (i != index) {
-          var dist = Math.sqrt();
+          var dx = label_array[i].x - label_array[index].x;
+          var dy = label_array[i].y - label_array[index].y;
+          var dist = Math.sqrt(dz * dx + dy * dy);
+          ener += 5;
         }
       }
   };
 
+  anneal.mcmove = function() {
+
+      // random number between 0 and 
+      var i = Math.floor((Math.random()*label_array.length)); 
+
+      // console.log(label_array[i].name)
+
+      var x_old = label_array[i].x;
+      var y_old = label_array[i].y;
+
+      var old_energy, new_energy;
+
+      label_array[i].x += (Math.random() - 0.5) * 50;
+      label_array[i].y += (Math.random() - 0.5) * 50;
+
+      // hard wall boundaries
+      if (label_array[i].x > w) label_array[i].x = x_old;
+      if (label_array[i].x < 0) label_array[i].x = x_old;
+      if (label_array[i].y > h) label_array[i].y = y_old;
+      if (label_array[i].y < 0) label_array[i].y = y_old;
+  };
+
   anneal.sweep = function() {
       var m = label_array.length;
-      for (var i = 0; i < m; i++) {
-          // console.log(label_array[i].name)
-
-          var x_old = label_array[i].x;
-          var y_old = label_array[i].y;
-
-          var old_energy, new_energy;
-
-          label_array[i].x += (Math.random() - 0.5) * 500;
-          label_array[i].y += (Math.random() - 0.5) * 500;
-
-          // hard wall boundaries
-          if (label_array[i].x > w) label_array[i].x = x_old;
-          if (label_array[i].x < 0) label_array[i].x = x_old;
-          if (label_array[i].y > h) label_array[i].y = y_old;
-          if (label_array[i].y < 0) label_array[i].y = y_old;
-      }
+      for (var i = 0; i < m; i++) { anneal.mcmove(); }
   };
 
   anneal.width = function(x) {
